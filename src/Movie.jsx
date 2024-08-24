00dd0e2a48ee81457aa4f23de3a2e9843e529975
O'Neil Obidiaso <obidiasodumebi@gmail.com>
2024-08-24 14:40:09 +0100

    Usuage of props, desturing, arrays and object in react

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-const director = "Abigail";
+import React from "react";
+
 const style = {
   border: "1px solid white",
 };
-const Movie = () => {
+
+const Movie = ({ name, image, genre, rating, Trend, director, alt }) => {
   return (
     <div>
-    <img
-      src="https://resizing.flixster.com/OBCMbeYEWS5tI2QhErFskxKyEa0=/206x305/v2/https://resizing.flixster.com/mPJp85eApHd8ih9XF5E9d3-2LbM=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzUxODlkZDE1LTQyYjUtNDg5ZS05NjZmLWMxZDk1YWZhN2E1ZC5qcGc="
-      alt="deadpool and wolverine movie image"
-    />
-    <h3>Deadpool and Wolverine</h3>
-    <p>Action, Fantasy</p>
-    <h5>7.8</h5>
-    <p>Director: {director}</p>
-  </div>
-  )
-}
+      <img src={image} alt={alt} />
+      <h3>{name}</h3>
+      <p>{genre}</p>
+      <h5>{rating}</h5>
+      <p>Director: {director}</p>
+      <p>{Trend}</p>
+    </div>
+  );
+};
 
-export default Movie
+export default Movie;
---
+++ src/movie db.js
@@ -0,0 +1,39 @@
+const movies = [
+    {
+      name: "Deadpool and Wolverine",
+      genre: "Action and Fantasy",
+      image:
+        "https://resizing.flixster.com/OBCMbeYEWS5tI2QhErFskxKyEa0=/206x305/v2/https://resizing.flixster.com/mPJp85eApHd8ih9XF5E9d3-2LbM=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzUxODlkZDE1LTQyYjUtNDg5ZS05NjZmLWMxZDk1YWZhN2E1ZC5qcGc=",
+      alt: "Deadpool and Wolverine",
+      rating: 7.8,
+      director: "Abigail",
+    },
+    {
+      name: "The Union movie image",
+      genre: "Action and Fantasy",
+      image:
+        "https://resizing.flixster.com/h_VUrsfh-P-2_kRDCNA_sFntBWk=/206x305/v2/https://resizing.flixster.com/hYB41f7m0pUhlpCmBVCbsq9LOCo=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzc0OGY3MDJiLTUzMTMtNDNiNC05MjgyLTM3ZWRmMzYzNTI0Ny5qcGc=",
+      alt: "The Union movie image",
+      rating: 4.1,
+      director: "Drink Water",
+    },
+    {
+      name: "TRAP",
+      genre: "Comedy, Horror",
+      image:
+        "https://resizing.flixster.com/g0rxvArZsZFsV8-gKSFPeNrmB-s=/206x305/v2/https://resizing.flixster.com/JMUezA59v3r50XvD34SELkqBnL0=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzZjYmVjMTllLTViOWYtNDY4NC04YWY5LTdmMGIxMTM1MWEwMS5qcGc=",
+      alt: "Trap movie image",
+      rating: 5.5,
+      director: "Stones",
+    },
+    {
+      name: "JACKPOT!",
+      genre: "Comedy, Business",
+      image:
+        "https://resizing.flixster.com/_yC8AF-46nLToYSY6v606xkp7dE=/206x305/v2/https://resizing.flixster.com/cJ-ZXvAojFDjeYi1VwAIOX2CNSE=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2EwMDgwMjUwLTQwMGYtNGViMi05MTEyLTAzMDYxMTkxZTI4ZS5qcGc=",
+      alt: "Jackpot! movie image",
+      rating: 3.2,
+      director: "Rat",
+    },
+  ];
+  export default movies;
